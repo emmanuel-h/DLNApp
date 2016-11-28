c84af77000ce01fe9c4444926af5e97dd17d67dc
benoitgallet <benoit.gallet@live.fr>
2016-11-28 15:10:11 +0100

    Update MainActivity.java

--- app/src/main/java/androidtd/dlnapp/MainActivity.java
+++ app/src/main/java/androidtd/dlnapp/MainActivity.java
@@ -2,6 +2,7 @@ package androidtd.dlnapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import une.classe.bidon;
 
 public class MainActivity extends AppCompatActivity {
 
