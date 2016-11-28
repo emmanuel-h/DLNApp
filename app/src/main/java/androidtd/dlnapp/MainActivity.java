a74641ec71b3e0ef3a9bf12cf6921e09fb8c0604
pidouGH <bryce.marbois@laposte.net>
2016-11-28 15:22:25 +0100

    test bryce

--- app/src/main/java/androidtd/dlnapp/MainActivity.java
+++ app/src/main/java/androidtd/dlnapp/MainActivity.java
@@ -2,7 +2,6 @@ package androidtd.dlnapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-//import du.swag.parce.que.j.en.ai.pas;
 
 public class MainActivity extends AppCompatActivity {
 
